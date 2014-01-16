9cfc39d22fc0e6d9d208bdb512e8f4a377522dc7
tarari <t.jimenez@escolesnuria.cat>
2014-01-16 16:00:51 +0100

    Delete config.php

--- application/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $config = Config::singleton();
-    $config->set('dbhost', 'localhost');
-    $config->set('dbname', 'toni_agencia');
-    $config->set('dbuser', 'toni');
-    $config->set('dbpass', '2001odisea');
-    
-?>
